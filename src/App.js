a034ecdfee414cb7f887ca63431bc82ca4df6df4
jaymesC <clementjaymes@gmail.com>
2021-10-23 14:34:25 +0100

    added app component files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import "./index.css";
+import { useQuery, gql } from "@apollo/client";
+import { useState } from "react";
+
+export const AnimeList = gql`
+query Query {
+  Page {
+    media {
+      siteUrl
+      title {
+        english
+        native
+      }
+      description
+      coverImage {
+        medium
+      }
+      bannerImage
+      volumes
+      episodes
+    }
+  }
+}
+`;
 
 function App() {
- 
-
-
-
+  const [page, setPage] = useState (1)
 
   return <div></div>;
 }
